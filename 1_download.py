955276837164f70a36874529d4131d31422c874f
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-19 15:14:43 +0000

    Removed names without sequences, simplified download

--- 1_download.py
+++ 1_download.py
@@ -33,15 +33,15 @@ sequences = download(names)  # returns dictionary of named sequences
 
 # OUTPUT
 counter = 0
-for name in sequences.keys():
-    seq = sequences[name]  # extract sequence
-    if seq:
-        seq.name = name  # make sure seq name is name
-        seq.id = name  # make sure seq id is name
-        seq = seq.format('fasta')  # convert to fasta format
-        with open(os.path.join(outdir, "{0}.fasta".format(name)), 'wb') as f:
+with open(os.path.join(outdir, "sequences.fasta"), 'wb') as f:
+    for name in sequences.keys():
+        seq = sequences[name]  # extract sequence
+        if seq:
+            seq.name = name  # make sure seq name is name
+            seq.id = name  # make sure seq id is name
+            seq = seq.format('fasta')  # convert to fasta format
             f.write("{0}\n".format(seq))
-        counter += 1
+            counter += 1
 
 # FINISH MESSAGE
 print("Downloaded [{}] sequences. Finished: [{}]".format(counter, timestamp()))
