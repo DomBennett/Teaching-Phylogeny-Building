b4920e2c23ea224c762fe374801db9eadc329c2f
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-19 16:36:00 +0000

    Created functions for stages 2 and 3

--- tools.py
+++ tools.py
@@ -1,12 +1,17 @@
 #! /bin/usr/env python
-# [YOUR NAME]
-# [DATE]
+# Dom Bennett
+# 19/02/2015
 # Tools for phylogeny-building pipeline
 
+import os
+import re
 from Bio import Entrez  # talk to NCBI's Entrez
 from Bio import SeqIO  # read sequences
 from random import sample  # random sample function
+from random import randint  # random number generator
 from datetime import datetime  # for timestamps
+from Bio.Align.Applications import MafftCommandline  # MAFFT command-line
+from Bio.Phylo.Applications import RaxmlCommandline  # RAxML command-line
 
 
 # FUNCTIONS
@@ -51,3 +56,50 @@ def download(names):
         # add to dictionary of name and sequence
         sequences[name] = result
     return sequences
+
+
+def align(infile, outfile, logfile):
+    '''Write MAFFT generated alignment to outfile from sequences in infile.
+    Return MAFFT log.'''
+    # use 'which' to find your mafft
+    # generate a command-line
+    cline = MafftCommandline(input=infile, auto=True)
+    # print the commandline that will be used
+    print 'Using command-line:\n.... {0}'.format(cline)
+    # run command-line
+    stdout, stderr = cline()
+    # write-out results
+    with open(outfile, 'w') as f:
+        f.write(stdout)
+    with open(logfile, 'w') as f:
+        f.write(stderr)
+    return stderr
+
+
+def estimate(infile, outfile, logfile):
+    '''Write RAxML generated tree to outfile from alignment in infile.
+    Return RAxML log.'''
+    cline = RaxmlCommandline(cmd='raxml', sequences=infile, model="GTRGAMMA",
+                             parsimony_seed=randint(0, 10000000),
+                             name='temp', threads=2)
+    # print the commandline that will be used
+    print 'Using command-line:\n.... {0}'.format(cline)
+    # run command-line
+    stdout, stderr = cline()
+    # move best tree to outfile
+    os.rename('RAxML_bestTree.temp', outfile)
+    # remove raxml outfiles
+    files = os.listdir(os.getcwd())
+    for file in files:
+        if re.search("(RAxML)", file):
+            os.remove(file)
+        if re.search("\.reduced$", file):
+            os.remove(file)
+    # write-out
+    if stdout:
+        with open(logfile, 'w') as f:
+            f.write(stdout)
+        return stdout
+    with open(logfile, 'w') as f:
+        f.write(stderr)
+    return stderr
