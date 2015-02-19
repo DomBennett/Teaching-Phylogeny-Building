497cc795d943c8981a9c93b06b221dbf43fa02f9
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-19 16:35:44 +0000

    Created estimate stage

--- 3_estimate.py
+++ 3_estimate.py
@@ -1,19 +1,27 @@
 #! /bin/usr/env python
-# [YOUR NAME]
-# [DATE]
+# Dom Bennett
+# 19/02/2015
 # Generate trees with RAxML
 
 # PACKAGES
-from Bio import AlignIO  # read alignments
+import os
+from tools import estimate
+from tools import timestamp
 
 # START MESSAGE
+print("Estimate stage, started: [{0}]".format(timestamp()))
 
 # DIRS
-
-# INPUT
+indir = '2_align'
+outdir = '3_estimate'
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
 
 # ESTIMATE
-
-# OUTPUT
+log = estimate(infile=os.path.join(indir, 'alignment.fasta'),
+               outfile=os.path.join(outdir, 'best_tree.tre'),
+               logfile=os.path.join(outdir, 'RAxML_log.txt'))
+print(log)
 
 # FINISH MESSAGE
+print("Finished: [{0}]".format(timestamp()))
