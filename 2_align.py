a45a3092451f96865e3c3a828e3b7446a895c42c
Dominic Bennett <dominic.john.bennett@gmail.com>
2015-02-19 16:35:31 +0000

    Created align stage

--- 2_align.py
+++ 2_align.py
@@ -1,19 +1,28 @@
 #! /bin/usr/env python
-# [YOUR NAME]
-# [DATE]
+# Dom Bennett
+# 19/02/2015
 # Align sequences
 
 # PACKAGES
-from Bio import SeqIO  # read sequences
+import os
+from tools import align
+from tools import timestamp
+
 
 # START MESSAGE
+print("Align stage, started: [{0}]".format(timestamp()))
 
 # DIRS
-
-# INPUT
+indir = '1_download'
+outdir = '2_align'
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
 
 # ALIGN
-
-# OUTPUT
+log = align(infile=os.path.join(indir, 'sequences.fasta'),
+            outfile=os.path.join(outdir, 'alignment.fasta'),
+            logfile=os.path.join(outdir, 'mafft_log.txt'))
+print(log)
 
 # FINISH MESSAGE
+print("Finished: [{0}]".format(timestamp()))
